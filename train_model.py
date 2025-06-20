2f33ac2543ab03b4f676bd2ca34f21416c3e05f2
rodrangal <rodrangal@gmail.com>
2025-06-20 17:15:30 -0400

    Update

--- train_model.py
+++ train_model.py
@@ -20,7 +20,7 @@ X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
 # Train/test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
 
 # Train model
 model = LogisticRegression()
